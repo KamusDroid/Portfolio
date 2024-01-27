d9115994671f12384c0cc48f8383f0de6c6bdddb
MatiasAmengual <waltermatiasamengual@gmail.com>
2024-01-27 16:29:57 -0300

    navlink

--- portfolio-web/src/app/page.js
+++ portfolio-web/src/app/page.js
@@ -4,7 +4,8 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black ">
-      <div class="container mx-auto px-12 py-4">
+      <Navbar />
+      <div class="container mt-24 mx-auto px-12 py-4">
 
         <HeroSection />
 
